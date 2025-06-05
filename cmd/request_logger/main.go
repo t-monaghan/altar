54e9a04154226bc0fabf41729492f7c85232690b
Tom Monaghan <62273348+t-monaghan@users.noreply.github.com>
2025-06-05 10:23:54 +0900

    changes logging in request logger to use slog (#6)

--- cmd/request_logger/main.go
+++ cmd/request_logger/main.go
@@ -1,69 +1,41 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
-	"time"
 )
 
 func requestLoggerHandler(w http.ResponseWriter, r *http.Request) {
-	// Log request details
-	fmt.Printf("[%s] %s %s from %s\n", time.Now().Format(time.RFC3339), r.Method, r.URL.Path, r.RemoteAddr)
-
-	// Read and log the request body
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		fmt.Printf("Error reading body: %v\n", err)
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		slog.Error("error reading body", "error", err)
+		http.Error(w, "error reading request body", http.StatusInternalServerError)
 
 		return
 	}
+	slog.Info("request received", "method", r.Method, "path", r.URL.Path, "body", body, "queryParamaters", r.URL.RawQuery)
 
-	// Log the body
-	if len(body) > 0 {
-		fmt.Printf("Body: %s\n", string(body))
-	} else {
-		fmt.Println("Body: <empty>")
-	}
-
-	// Log headers
-	// fmt.Println("Headers:")
-	// for name, values := range r.Header {
-	// 	for _, value := range values {
-	// 		fmt.Printf("  %s: %s\n", name, value)
-	// 	}
-	// }
-
-	fmt.Println("-----------------------------------")
-
-	// TODO: log the query paramaters
-
-	// Send a simple response
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Request logged successfully"))
+	w.Write([]byte("request logged successfully"))
 }
 
 func main() {
 	port := "8080"
 
-	// Use custom port if provided as argument
 	if len(os.Args) > 1 {
 		port = os.Args[1]
 	}
 
-	// Create handler for all paths
 	http.HandleFunc("/", requestLoggerHandler)
 
-	// Start the server
 	addr := ":" + port
-	fmt.Printf("Starting request logger server on %s\n", addr)
-	fmt.Println("Press Ctrl+C to stop")
+	slog.Info("starting request logger server on " + addr)
 
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+		log.Fatalf("failed to start server: %v", err)
 	}
 }
